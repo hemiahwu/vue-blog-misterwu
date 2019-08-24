335bc496d3f8f40d7d9dd383e225b98606848990
hemiahwu <309595700@qq.com>
2019-08-24 10:41:23 +0800

    'publish'

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Header />
+
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/mixins/searchMixins.js
@@ -0,0 +1,9 @@
+export default {
+    computed: {
+        filterdBlogs() {
+            return this.blogs.filter(blog => {
+                return blog.title.match(this.search);
+            });
+        }
+    }
+}
